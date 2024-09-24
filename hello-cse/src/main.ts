c8ac85ad13d14af18a11a5a004e72495974642a1
clement moreno <clementmoreno0803@gmail.com>
2024-09-24 13:59:47 +0200

    ajout config babel + package.json + organisation dossier archi hexagonale

--- hello-cse/src/main.ts
+++ hello-cse/src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { createVuetify } from "vuetify";
+import "vuetify/styles";
 import App from "./App.vue";
 import router from "./router";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-createApp(App).use(router).mount("#app");
+const pinia = createPinia();
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(router).use(vuetify).use(pinia).mount("#app");
